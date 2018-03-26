6d996328db925b3761c3445b0dc7cb18ddfae8cc
VincentCastig <vincent.castig@gmail.com>
2018-03-26 18:27:03 -0500

    Reducers

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './Reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
