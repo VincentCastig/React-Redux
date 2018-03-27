97584f0b01f0e51df34495bed33b16705ef95b77
VincentCastig <vincent.castig@gmail.com>
2018-03-26 21:55:32 -0500

    Call Action on Click

--- book-list/src/containers/book-list.js
+++ book-list/src/containers/book-list.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions';
+//bindActionCreators makes sure the action selectBook return value flows through all our reducers
+import { bindActionCreators } from 'redux';
 
 class Booklist extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title}className="list-group-item">
+                <li 
+                    key={book.title} 
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
                     {book.title}
                 </li>
             )
@@ -13,6 +19,7 @@ class Booklist extends Component {
     }
 
 render() {
+    console.log(this.props)
     return (
         <ul className="list-group col-sm-4">
             {this.renderList()}
@@ -29,4 +36,10 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(Booklist);
+//anything return from this function will end up as props on the booklist container
+function mapDispatchToProps(dispatch) {
+    //Whatever selectBook is called, the result should be passed to all our reducers
+    return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+//Promote BookList from a component to a container
+export default connect(mapStateToProps, mapDispatchToProps)(Booklist);
