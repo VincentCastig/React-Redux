33c2732ee3c43566d85f357de43e8d01904306f9
VincentCastig <vincent.castig@gmail.com>
2018-03-30 15:10:14 -0400

    Display Temp, Pressure, and Humidity Data

---
+++ weather/src/Components/chart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chart = ({data}) => {
+    return (
+        <div>
+            {data}
+        </div>
+    )
+}
+
+export default Chart;
--- weather/src/containers/weather-list.js
+++ weather/src/containers/weather-list.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions'; 
+import Chart from '../Components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
     console.log(temps)
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-            {temps[0]}
-            {temps[1]}
-            {temps[2]}
-            {temps[3]}
+          <Chart 
+          data={pressure[0]}/>  
+        </td>
+        <td>
+          <Chart 
+          data={pressure[0]}/>  
+        </td>
+        <td>
+          <Chart 
+          data={humidity[0]}/>  
         </td>
       </tr>
     )
